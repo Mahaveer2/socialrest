8562e0b7ac5f92cd93c88b03fe788213c06eb9eb
Partab Rai <amrani.partab@gmail.com>
2022-12-01 12:12:26 +0500

    something in the way

--- connect.js
+++ connect.js
@@ -1,8 +1,17 @@
-import mysql from "mysql";
+import mysql from "mysql2";
+
+
+let MYSQLDATABASE = "railway";
+let MYSQLUSER = "root";
+let MYSQLHOST = "containers-us-west-101.railway.app";
+let MYSQLPORT = "6859";
+let MYSQLPASSWORD = "mehiyVnoODYCG4gAkxkv";
 
 export const db = mysql.createConnection({
   host:'containers-us-west-101.railway.app',
   user:'root',
   password:"mehiyVnoODYCG4gAkxkv",
-  database:"railway"
+  database:"railway",
+  port:"6859",
+  connectionLimit : 100,
 });
